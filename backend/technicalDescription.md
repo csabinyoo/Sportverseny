806ddeab00e734f13fb3d0d1a0eae4f715611966
csabinyoo <mdgycsba@gmail.com>
2025-01-30 11:21:45 +0100

    *

---
+++ backend/technicalDescription.md
@@ -0,0 +1,14 @@
+<p class="title"> Adatbázis [<span class="alt-title">Database</span>]
+
+# Táblák [Tables]
+
+<style>
+.title
+{
+    font-size: 50px;
+    text-align: center;
+}
+.alt-title {
+    color: #777;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+    <h1 class="text-center">Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+
+</style>
