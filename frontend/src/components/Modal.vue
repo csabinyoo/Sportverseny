763b75de12214c9770e332ef38596e7fdc0a2431
csabinyoo <mdgycsba@gmail.com>
2025-01-30 10:30:44 +0100

    *

--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -1,66 +1,62 @@
 <template>
+  <div class="modal fade" id="modal" tabindex="-1">
     <div
-      class="modal fade"
-      id="modal"
-      tabindex="-1"
+      class="modal-dialog modal-dialog-centered"
+      :class="{
+        'modal-xl': size == 'xl',
+        'modal-lg': size == 'lg',
+        'modal-sm': size == 'sm',
+      }"
     >
-      <div
-        class="modal-dialog modal-dialog-centered"
-        :class="{
-          'modal-xl': size == 'xl',
-          'modal-lg': size == 'lg',
-          'modal-sm': size == 'sm',
-        }"
-      >
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">{{ title }}</h1>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <slot></slot>
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
-              v-if="no"
-            >
-              {{ no }}
-            </button>
-            <button
-              type="button"
-              class="btn btn-primary"
-              v-if="yes"
-              @click="onClickYesButton()"
-              data-bs-dismiss="modal"
-            >
-              {{ yes }}
-            </button>
-          </div>
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">{{ title }}</h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <slot></slot>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+            v-if="no"
+          >
+            {{ no }}
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            v-if="yes"
+            @click="onClickYesButton()"
+            data-bs-dismiss="modal"
+          >
+            {{ yes }}
+          </button>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
   <script>
-  export default {
-    props: ["title", "yes", "no", "size"],
-    emits: ["yesEvent"],
-  
-    methods: {
-      onClickYesButton() {
-        this.$emit("yesEvent");
-      }
+export default {
+  props: ["title", "yes", "no", "size"],
+  emits: ["yesEvent"],
+
+  methods: {
+    onClickYesButton() {
+      this.$emit("yesEvent");
     },
-  };
-  </script>
+  },
+};
+</script>
   
   <style>
-  </style>
+</style>
